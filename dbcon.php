27dabdeef5f75b3507b8416098e70c574bbe3a1c
Sarbkalyan Singh <sarbbsandhu555@gmail.com>
2021-09-19 09:20:42 +0530

    done

---
+++ dbcon.php
@@ -0,0 +1,26 @@
+<?php
+
+    $server  = "localhost";
+    $user = "root";
+    $password = "";
+    $db = "signup11";
+
+    $conn = mysqli_connect($server,$user,$password,$db);
+
+    if($conn)
+    {
+      ?>
+    <script>
+            alert(" Connection  Done");
+    </script>
+    <?php
+    }else
+    {
+        ?>
+        <script>
+            alert(" Connection not successful ");
+        </script>
+        <?php
+    }
+
+?>
