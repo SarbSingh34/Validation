54c1d892043f88702860705f215003db3061485c
Sarbkalyan Singh <sarbbsandhu555@gmail.com>
2021-09-22 17:16:10 +0530

    done

--- logout.php
+++ logout.php
@@ -3,6 +3,11 @@
     session_start();
 
     session_destroy();
+
+    setcookie('emailcookie','',time()-86400);
+    setcookie('passwordcookie','',time()-86400);
+
+
     header('location:login.php');
    
 ?>
