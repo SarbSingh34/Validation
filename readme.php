504b8e5f2974028412c707c50eb6634e227a359c
Sarbkalyan Singh <sarbbsandhu555@gmail.com>
2021-09-22 19:15:11 +0530

    done

---
+++ readme.php
@@ -0,0 +1,6 @@
+<!-- Account is created for registration and  different pg folowed -->
+<!-- Login and Logout part is made to full functionality  -->
+<!-- Mail is sent for USER  verification and then account updated   -->
+<!-- Remember me checkboxx part - tick it and evertime u want to login Automatically it filled the Login and password  -->
+<!--    We used Cookies for that  , Specific  Time for their Storage   -->\
+<!-- Next Part for Forget Password is on Next Branch(Forget_Password)............... -->
